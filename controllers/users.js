5ceac94d064e0cd122ca4aed8a6ce8430fcc24f2
mblester <morganmurry@gmail.com>
2022-06-21 19:22:36 -0500

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const users = require('../data/index')
+
+const listUsers = (req, res) => {
+    res.json(users)
+}
+
+const showUser = (req, res) => {
+    
+}
+
+const createUser = (req, res) => {
+    
+}
+
+const updateUser = (req, res) => {
+    
+}
+
+const deleteUser = (req, res) => {
+    
+}
+
+module.exports={listUsers, showUser, createUser, updateUser, deleteUser}
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+//const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 4000
+const usersRoute = require('./route/users')
+
+//app.use(bodyParser())
+app.use(express.json())
+app.use(usersRoute)
+
+app.get('/', (req, res) => res.send('default route'))
+
+app.listen(port, () => {
+  console.log('app is listening on:', port)
+})
