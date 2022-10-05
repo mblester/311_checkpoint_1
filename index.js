bb7fbbaeb99267cbc68e02357c2dcff06b269e3c
mblester <morganmurry@gmail.com>
2022-10-04 20:07:17 -0500

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 
 const appointmentsRouter = require("./routes/appointments");
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
